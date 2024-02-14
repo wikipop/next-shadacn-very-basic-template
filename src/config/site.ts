7243108c264b013e5653cf768e12775d9c711aa5
wikipop <66514885+wikipop@users.noreply.github.com>
2024-02-14 12:12:10 +0100

    initial commit

---
+++ src/config/site.ts
@@ -0,0 +1,21 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+    author: {
+        name: "Wiktor Popiołek",
+    },
+    name: "template",
+    description:
+        // TODO: Change this description
+        "template used by me",
+    mainNav: [
+        {
+            title: "Home",
+            href: "/",
+        },
+    ],
+    links: {
+        instagram: "https://www.instagram.com/wiktor_popiolek/",
+        github: "https://github.com/wikipop/wyniki-zip-proj",
+    },
+}
